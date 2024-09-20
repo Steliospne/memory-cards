72196468f8e264abcab64b8e819e5c86d75af239
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-09-20 15:18:10 +0200

    Playing around to get used to nextJS app file stracture

--- app/game/page.js
+++ app/game/page.js
@@ -1,8 +1,16 @@
-export default function Game() {
+import Link from "next/link";
+import { capitalize } from "../lib/functions";
+import Image from "next/image";
+import backButtonLogo from "@/public/assets/arrow.png";
+
+export default function Game({ searchParams }) {
   return (
-    <>
-      <h1>This is the Game Page</h1>
-      <a href='/'>Back</a>
-    </>
+    <main>
+      <h1>This is the Game Page in {capitalize(searchParams.mode)}</h1>
+      <Link className='back-btn' href={"/"}>
+        Back
+        <Image alt='back button logo' width={64} src={backButtonLogo} />
+      </Link>
+    </main>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
+import Image from "next/image";
+import logo from "@/public/assets/logo-L.png";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>{"MAIN"}</h1>
-        <a href='/game'>Game</a>
-      </main>
-      <footer>FOOTER</footer>
-      <video autoPlay loop>
-        <source src='test.mp4' type='video/mp4' />
-      </video>
-    </>
+    <main>
+      <Image className='logo' width={750} alt='logo' src={logo} />
+      <div className='btn-wrapper'>
+        <Link href={{ pathname: "/game", query: { mode: "easy" } }}>Easy</Link>
+        <Link href={{ pathname: "/game", query: { mode: "medium" } }}>
+          Medium
+        </Link>
+        <Link href={{ pathname: "/game", query: { mode: "hard" } }}>Hard</Link>
+      </div>
+    </main>
   );
 }
