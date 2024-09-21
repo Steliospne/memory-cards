b21c6d198988f91b9833a2d9e19ada893a47fd06
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-09-21 13:38:30 +0200

    Adds configuration settings for loading mp3s

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: "file-loader",
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? "../" : ""}static/images/`,
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
