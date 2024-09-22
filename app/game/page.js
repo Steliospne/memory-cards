243df53e4c3054ac9281a592db2470a2a7988c6e
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-09-22 07:38:40 +0200

    Adds the game page with some placeholder text and a back button

--- app/game/page.js
+++ app/game/page.js
@@ -1,13 +1,20 @@
+"use client";
 import Link from "next/link";
-import { capitalize } from "../lib/functions";
+import { capitalize, playClick } from "../lib/functions";
 import Image from "next/image";
 import backButtonLogo from "@/public/assets/arrow.png";
+import { useSettings } from "../components/provider";
 
 export default function Game({ searchParams }) {
+  const { settings, playClick } = useSettings();
   return (
     <main>
       <h1>This is the Game Page in {capitalize(searchParams.mode)}</h1>
-      <Link className='back-btn' href={"/"}>
+      <Link
+        onClick={() => playClick(settings.sound_effects)}
+        className='back-btn'
+        href={"/"}
+      >
         Back
         <Image alt='back button logo' width={64} src={backButtonLogo} />
       </Link>
