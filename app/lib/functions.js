8922f4830c5e4bd842c044f2137c5ab4d7524a8e
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-09-22 13:36:18 +0200

    Adds two new functions the shuffle and getChampions the first shuffles any array and the second return an array of champion objects

--- app/lib/functions.js
+++ app/lib/functions.js
@@ -9,4 +9,39 @@ export const capitalize = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
-// const clickAudio = new Audio(clickSound);
+export const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const swappingIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[swappingIndex]] = [array[swappingIndex], array[i]];
+  }
+  return array;
+};
+
+export const getChampions = (array, mode = "easy") => {
+  const selectedChampions = [];
+  let numberOfChampions = 5;
+
+  switch (mode) {
+    case "easy":
+      numberOfChampions = 5;
+      break;
+    case "medium":
+      numberOfChampions = 8;
+      break;
+    case "hard":
+      numberOfChampions = 10;
+      return array;
+    default:
+      numberOfChampions = 5;
+      break;
+  }
+  for (let i = 0; i < numberOfChampions; i++) {
+    let randomIndex = Math.floor(Math.random() * (array.length - 1));
+
+    while (selectedChampions.includes(array[randomIndex])) {
+      randomIndex = Math.floor(Math.random() * (array.length - 1));
+    }
+    selectedChampions.push(array[randomIndex]);
+  }
+  return selectedChampions;
+};
