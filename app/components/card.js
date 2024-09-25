e27bc6eb6b606b7fbfe89eb16c31fa00deb21f0c
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-09-25 05:55:55 +0200

    Adds framer-motion and implements a small animation for the shuffle

--- app/components/card.js
+++ app/components/card.js
@@ -1,11 +1,18 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 function Card({ champion, onClick }) {
   return (
-    <div className='card' id={champion.name} onClick={onClick}>
+    <motion.div
+      layout
+      transition={{ type: "tween", duration: 1 }}
+      className='card'
+      id={champion.name}
+      onClick={onClick}
+    >
       <Image alt='champion logo' src={champion.img} width={100} />
       <h4>{champion.name}</h4>
-    </div>
+    </motion.div>
   );
 }
 
